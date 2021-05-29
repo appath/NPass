ed246d7192440a87ec9dcb33e731ed3feb02d0a3
Boris <htc.boko@gmail.com>
2021-05-29 21:43:59 +0300

    Update ___npass_console.py

--- ___npass_console.py
+++ ___npass_console.py
@@ -1,54 +1,42 @@
 #!/usr/bin/env python3
+import uuid
+import random
 import argparse
-from uuid import uuid4
-from os import name, system
-from random import choice, SystemRandom
 
-parser = argparse.ArgumentParser(prog="NPass Console Option", 
-formatter_class=argparse.RawDescriptionHelpFormatter, 
-description="""\
-    README
+parser = argparse.ArgumentParser(prog="NPass Console", formatter_class=argparse.RawDescriptionHelpFormatter, description="""\
     To prevent your passwords from being hacked by social engineering, 
     brute force or dictionary attack method, and keep your
     online accounts safe, you should notice that
-
+    
     * Always use a unique password for each account.
     * Do not use personal information in your passwords. Names, birthdays and the like.
     * Passwords must be at least 15, contain letters, numbers, symbols
     * Avoid fairly weak, and commonly used passwords
     * Do not use similar passwords that change only one word or any other character.
-
-        2020 Wizard Packed, Free Software
-        Git: https://github.com/appath/NPass""")
-
-parser.add_argument("-v", "--version", action="version", help="print version", version="%(prog)s [version 0.2.1] 10/08/2020")
-parser.add_argument("-l", dest="length", type=int, help="length of password in integer, default is [8]", default=8)
+    
+    2020 Wizard Packed, Free Software
+    GitHub: https://github.com/appath""")
+parser.add_argument("-v", "--version", help="print version", action="version", version="%(prog)s [version 1.12.0]")
+parser.add_argument("-l", dest="length", type=int, help="length of password in integer, default is 8", default=8)
 parser.add_argument("-c", dest="count", type=int, help="number of passwords to generate", default=1)
-args = parser.parse_args()
+(options) = parser.parse_args()
+length = options.length
+count = options.count
 
-def get_info():
-    print(f"\nGenerate [{count}] a random string of fixed length")
-    print(f"Use the sampling function when you don’t want to repeat [{length}] characters in a random")
-    print("=" * 32 + "\n")
-
-def get_passwords():
+def get_password():
+    salt = uuid.uuid4().hex
     password_characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    salt = uuid4().hex
-    SR = SystemRandom()
-    return "".join(SR.choice(password_characters + salt) for i in range(length))
+    return "".join(random.choice(salt + password_characters) for i in range(length))
+
+def info_password_0():
+    print("Strong passwords are unique and random.\n")
+
+def info_password_1():
+    print(f"\n:: Generate {count} a random string of fixed length")
+    print(f"   Use the sampling function when you don’t want to repeat {length} characters in a random . . .")
 
 if __name__ == "__main__":
-    try:
-        length = args.length
-        count = args.count
-
-        get_info()
-
-        for str_count in range(count):
-            print(get_passwords())
-
-        input("\nPress ENTER to exit()")
-        system("cls" if name == "nt" else "clear")
-
-    except KeyboardInterrupt:
-        system("cls" if name == "nt" else "clear")
+    info_password_0()
+    for str_count in range(count):
+        print(get_password())
+    info_password_1()
